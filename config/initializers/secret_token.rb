01aecc5eddd47a418ef56344010ea634bcc957b7
Michael Morrissey <mikehmorrissey@gmail.com>
2013-06-22 10:53:33 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunarmayor::Application.config.secret_token = '3240dd64ea3ea5c062115b27f624bce547af56e716aa87be5b60533f5deee1030f3918faea4b2bb2fbacc7ce9cbc204e0b579a4ec80f31bd8fbd44bcd4a32609'
